6b439f8979f99e5ab33f9a1f312dae997df307ce
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-07-24 16:37:34 +0200

    add a test program for the pysical size opening
    darcs-hash:20060724143734-2fc9d-5d7a5fb0fe62ce1ace95b1c1749a8d03a18e07b4.gz

---
+++ itkPhysicalSizeOpeningImageFilter.h
@@ -0,0 +1,64 @@
+#ifndef __itkPhysicalSizeOpeningImageFilter_h
+#define __itkPhysicalSizeOpeningImageFilter_h
+
+#include "itkAttributeMorphologyBaseImageFilter.h"
+
+namespace itk
+{
+template <class TInputImage, class TOutputImage>
+class ITK_EXPORT PhysicalSizeOpeningImageFilter :
+    public AttributeMorphologyBaseImageFilter<TInputImage, TOutputImage, typename TInputImage::SpacingType::ValueType, std::greater<typename TInputImage::PixelType> >
+
+{
+public:
+  typedef PhysicalSizeOpeningImageFilter Self;
+  typedef AttributeMorphologyBaseImageFilter<TInputImage, TOutputImage, typename TInputImage::SpacingType::ValueType, std::greater<typename TInputImage::PixelType> > Superclass;
+
+  typedef SmartPointer<Self>   Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /**
+   * Extract some information from the image types.  Dimensionality
+   * of the two images is assumed to be the same.
+   */
+  typedef typename TOutputImage::PixelType OutputPixelType;
+  typedef typename TOutputImage::InternalPixelType OutputInternalPixelType;
+  typedef typename TInputImage::PixelType InputPixelType;
+  typedef typename TInputImage::InternalPixelType InputInternalPixelType;
+  typedef typename TInputImage::IndexType IndexType;
+  typedef typename TInputImage::OffsetType OffsetType;
+  typedef typename TInputImage::SizeType SizeType;
+
+  itkStaticConstMacro(ImageDimension, unsigned int,
+                      TOutputImage::ImageDimension);
+  
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  virtual ~PhysicalSizeOpeningImageFilter() {}
+protected:
+  PhysicalSizeOpeningImageFilter(){}
+  void GenerateData()
+    {
+    // compute pixel size
+    this->m_AttributeValuePerPixel = 1;
+    for( int i=0; i<ImageDimension; i++)
+      {
+      this->m_AttributeValuePerPixel *= this->GetInput()->GetSpacing()[i];
+      }
+    // std::cout << "m_AttributeValuePerPixel: " << this->m_AttributeValuePerPixel << std::endl;
+    // and call superclass implementation of GenerateData()
+    Superclass::GenerateData();
+    }
+
+
+private:
+
+  PhysicalSizeOpeningImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+
+};
+
+} // namespace itk
+#endif
